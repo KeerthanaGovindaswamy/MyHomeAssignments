8f5de84766b2d6963a875bed990b6c6672f1536e
KeerthanaGovindaswamy <keerthanaravi0616@gmail.com>
2025-01-20 16:55:42 +0530

    Day1 Home assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		System.out.println("The version of chrome is :" + version);
+		String developer="Google";
+		System.out.println("The developer is :" +developer);
+		boolean isBeta=false;
+		System.out.println("The boolean type is :" +isBeta);
+		int releaseYear=2008;
+		System.out.println("The Release year is :" +releaseYear);
+		char shortCutKey ='c';
+		System.out.println("The shortcutkey is :" +shortCutKey);
+
+	}
+
+}
---
+++ day1/LearnDataTypes.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class LearnDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//float
+		float browserVersion=100.2f;
+		System.out.println(browserVersion);
+		//string
+		String browserName="Firefox";
+		System.out.println(browserName);
+		//boolean
+		boolean isVisible=true;
+		System.out.println(isVisible);
+		//int
+		int releaseYear=1998;
+		System.out.println(releaseYear);
+		//char
+		char browserLogo='f';
+		System.out.println(browserLogo);
+	//	System.out.println(browserVersion +" "+browserName+" "+isVisible+" "+releaseYear+" "+browserLogo);
+
+	}
+
+}
